f5d9a3860711a88a241b41eee1946958064739e1
Noelle Huchette <noelle.huchette@gmail.com>
2022-06-15 20:09:22 +0000

    completed 'modules'

---
+++ ls.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+module.exports = function() {
+    fs.readdir('./', (err, files) => {
+        if (err) {
+            throw err;
+        }
+        else {
+            process.stdout.write(files.join('\n'));
+            process.stdout.write('\nprompt > ');
+        }
+    });
+};
